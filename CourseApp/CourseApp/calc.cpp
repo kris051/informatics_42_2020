78c059362c4e70e677de5f0643b1cdc37e16b0a1
Кристина Пухова <kristina.pukhova.2002@mail.ru>
2020-10-30 21:14:37 +0300

    last changes in calc.cpp

--- CourseApp/CourseApp/calc.cpp
+++ CourseApp/CourseApp/calc.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <math.h>
-
+using namespace std;
 double calc(double a, double x)
 {
 	double numenator = pow(log(a + x), 2);
@@ -18,7 +18,7 @@ void taskA(double a, double xn, double xk, double dx)
 {
 	for (double x = xn; x <= xk; x += dx) {
 		double y = calc(a, x);
-		std::cout << x << " " << y << std::endl;
+		cout << x << "\t" << y << endl;
 	}
 	return;
 }
@@ -28,10 +28,7 @@ void taskB(double a, double* x, int size)
 	for (int i = 0; i < size; i++)
 	{
 		double y = calc(a, x[i]);
-		//printf;
-		char result[80];
-		sprintf_s(result, "x = %3.5f y = %.2f \n", x[i], y);
-		cout << result << endl;
+		cout << x[i] << "\t" << y << endl;
 
 	}
 }
